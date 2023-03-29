76121d2b0b599e8de86530410f93fe043652e9fe
Gaurav Mittal <121662892+mittal0706@users.noreply.github.com>
2023-03-30 00:10:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
-
+module "vpc_module" {
+ 
+  source = "./modules/aws_vpc"
+  
+  vpc_cidr_block = ""
+  
+  tags = ""
+}
+  
+module "aws_subnet" {
+  
+ source= "./modules/aws_subnets"
+  
+ vpc_id = ""
+  
+ cidr_block = ""
+  
+ availability_zone = ""
+  
+ tags = ""
+}
